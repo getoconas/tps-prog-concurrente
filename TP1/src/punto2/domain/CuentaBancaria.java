6d5c124a420b51d71008cec076a532f546438e1f
getoconas <getoconas@gmail.com>
2024-08-16 00:29:47 -0300

    tp1 punto2

---
+++ TP1/src/punto2/domain/CuentaBancaria.java
@@ -0,0 +1,54 @@
+package punto2.domain;
+
+public class CuentaBancaria {
+	private static long ultima_cuenta_creada = 100000;
+	protected long numero_cuenta;
+	protected long dni;
+	protected double saldo_actual;
+	protected double interes_anual;
+	
+	// Constructor por Defecto
+	public CuentaBancaria() {
+		this.numero_cuenta = ++ultima_cuenta_creada;
+		this.dni = 0;
+		this.saldo_actual = 0;
+		this.interes_anual = 0;
+	}
+	
+	// Constructor con dni, saldo_actual e interes_anual
+	public CuentaBancaria(long dni, double saldo_actual, double interes_anual) {
+		this.numero_cuenta = ++ultima_cuenta_creada;
+		this.dni = dni;
+		this.saldo_actual = saldo_actual;
+		this.interes_anual = interes_anual;
+	}
+	
+	// Actualiza el saldo actual de acuerdo al interes diario
+	public void actualizarSaldo() {
+		this.saldo_actual += this.saldo_actual * ((this.interes_anual / 100) / 365);
+	}
+	
+	// Ingresa una cantidad de dinero
+	public void ingresar(double cantidad) {
+		this.saldo_actual += cantidad;
+	}
+	
+	// Retira una cantidad de dinero
+	public void retirar(double cantidad) {
+		if (this.saldo_actual > 0 && cantidad <= this.saldo_actual) {
+			this.saldo_actual -= cantidad;
+		} else {
+			System.out.println("***** SALDO INSUFICIENTE *****");
+		}
+	}
+	
+	// Muestra información de la cuenta
+	public String toString() {
+		return "-----------" + "\n" +
+				"NUMERO DE CUENTA: " + this.numero_cuenta + "\n" +
+				"DNI: " + this.dni + "\n" +
+				"SALDO ACTUAL: " + String.format("%.2f", this.saldo_actual) + "\n" +
+				"INTERES ANUAL: " + this.interes_anual + "%\n" +
+				"-----------";
+	}
+}
---
+++ TP1/src/punto2/test/Main.java
@@ -0,0 +1,31 @@
+package punto2.test;
+
+import punto2.domain.CuentaSueldo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Incializacion de objetos
+		CuentaSueldo cuenta1 = new CuentaSueldo(25669742, 356991.55, 2.5, "BANCO MACRO", "BENEFICIO 1");
+		CuentaSueldo cuenta2 = new CuentaSueldo(38423011, 150399.04, 2.5, "BANCO NACION", "BENEFICIO 2");
+		CuentaSueldo cuenta3 = new CuentaSueldo(29770300, 98775.23, 2.5, "BANCO HSBC", "BENEFICIO 3");
+		CuentaSueldo cuenta4 = new CuentaSueldo(33479804, 254790.46, 2.5, "BANCO GALICIA", "BENEFICIO 4");
+		
+		// Mostrar informacion inicial
+		System.out.println(cuenta1.toString());
+		System.out.println(cuenta2.toString());
+		System.out.println(cuenta3.toString());
+		System.out.println(cuenta4.toString());
+		System.out.println("*****");
+		
+		// Retirar dinero
+		cuenta3.retirar(25000.00);
+		System.out.println("*****");
+		// Enviar transferencia por cbu
+		cuenta1.transferir(150000.00, 80559843417474329L);
+		System.out.println("*****");
+		// Enviar transferencia por alias
+		cuenta2.transferir(35000.00, "EL.MAGO");
+	}
+
+}
