c8feefc86241f6feb9764868a2ede203f01760ae
getoconas <getoconas@gmail.com>
2024-09-06 22:21:53 -0300

    tp1 punto1a

---
+++ TP3/src/punto1a/Main.java
@@ -0,0 +1,16 @@
+package punto1a;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.print("." + i + "X.");
+		}
+		System.out.println("");
+		for (int i = 0; i < 100; i++) {
+			System.out.print("." + i + "Y.");
+		}
+
+	}
+
+}
